0e0d8d94624282b85e7ce1533e193cf1258a4e86
tajbirahmed <tajbir26@gmail.com>
2024-08-16 19:47:41 +0600

    added product service

---
+++ server/MicroServices/product-service/src/main/java/com/techie/microservices/product/dto/ProductResponse.java
@@ -0,0 +1,11 @@
+package com.techie.microservices.product.dto;
+
+import java.math.BigDecimal;
+
+public record ProductResponse(
+        String id,
+        String name,
+        String description,
+        BigDecimal price
+) {
+}
