7c707392a80b7b0c4ff288bba537f9c7cc3ec0d4
tajbirahmed <tajbir26@gmail.com>
2024-08-18 01:02:17 +0600

    added order-service

---
+++ server/MicroServices/order-service/src/main/java/com/techie/microservices/order/dto/OrderRequest.java
@@ -0,0 +1,12 @@
+package com.techie.microservices.order.dto;
+
+import java.math.BigDecimal;
+
+public record OrderRequest(
+        Long id,
+        String orderNumber,
+        String skuCode,
+        BigDecimal price,
+        Integer quantity
+) {
+}
---
+++ server/MicroServices/order-service/src/main/java/com/techie/microservices/order/service/OrderService.java
@@ -0,0 +1,30 @@
+package com.techie.microservices.order.service;
+
+import com.techie.microservices.order.dto.OrderRequest;
+import com.techie.microservices.order.model.Order;
+import com.techie.microservices.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(
+            OrderRequest orderRequest
+    ) {
+//        map orderRequest to Order object
+//        save order to OrderReqpository
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price());
+        order.setSkuCode(orderRequest.skuCode());
+        order.setQuantity(orderRequest.quantity());
+        orderRepository.save(order);
+    }
+}
