d289345a295c17d86c0e68268c66bad04d5975c3
tajbirahmed <tajbir26@gmail.com>
2024-08-18 14:01:04 +0600

    added FeignClinet for service communication(synchronous)

--- server/MicroServices/order-service/src/main/java/com/techie/microservices/order/service/OrderService.java
+++ server/MicroServices/order-service/src/main/java/com/techie/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.techie.microservices.order.service;
 
+import com.techie.microservices.order.client.InventoryClient;
 import com.techie.microservices.order.dto.OrderRequest;
 import com.techie.microservices.order.model.Order;
 import com.techie.microservices.order.repository.OrderRepository;
@@ -14,17 +15,25 @@ public class OrderService {
 
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(
             OrderRequest orderRequest
     ) {
-//        map orderRequest to Order object
-//        save order to OrderReqpository
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
+        // We can mock this call using library Mockito
+        // or, Wiremock
+
+        var isProductIsInStok = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (isProductIsInStok) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
+        }
     }
 }
--- server/MicroServices/order-service/src/test/java/com/techie/microservices/order/OrderServiceApplicationTests.java
+++ server/MicroServices/order-service/src/test/java/com/techie/microservices/order/OrderServiceApplicationTests.java
@@ -1,5 +1,7 @@
 package com.techie.microservices.order;
 
+import com.techie.microservices.order.client.InventoryClient;
+import com.techie.microservices.order.stub.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,12 +9,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MySQLContainer;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
 	@ServiceConnection
@@ -36,11 +40,12 @@ class OrderServiceApplicationTests {
 
 		String submitOrderJson = """
 					{
-						  "skuCode": "iphone_15",
+						  "skuCode": "pixel_8",
 						  "price": 1000,
 						  "quantity": 1
 					 }
 				""";
+		InventoryClientStub.stubInventoryCall("pixel_8", 1);
 		var responseBodyString = RestAssured.given()
 				.contentType("application/json")
 				.body(submitOrderJson)
